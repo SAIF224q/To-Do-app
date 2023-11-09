2407315b5b5497dba816f4ac8c5e01898ff67164
Saif Ali <saif224q@gmail.com>
2023-11-09 13:47:38 +0530

    Add new feature for removing done tasks

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 let input = document.getElementById("input") ; 
 let list = document.getElementById("elements")
 
+
 function addElement(){
     let toDo_element = document.createElement("li")
+    let done_button = document.createElement("button")
+
     let inputValue = input.value
     toDo_element.innerHTML = inputValue;
-    list.appendChild(toDo_element);
+    done_button.innerText = "DONE";
+    toDo_element.setAttribute("class","to-do-element")
+    done_button.setAttribute("class","done");
+    list.append(toDo_element);
+    toDo_element.append(done_button)
     input.value = ""    
+
+    done_button.addEventListener("click", function () {
+        toDo_element.parentNode.removeChild(toDo_element);
+    });
 }
+
+
+
+
+
